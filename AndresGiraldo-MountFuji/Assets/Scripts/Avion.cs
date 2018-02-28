e8112e016c3c8ce105397d872d33a93ef2ff4fe6
Andres Giraldo <andresgir0390@gmail.com>
2018-02-28 15:05:59 -0500

    Monte Fuji :D

---
+++ AndresGiraldo-MountFuji/Assets/Scripts/Avion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avion : MonoBehaviour {
+	public float sx;
+	public float sy;
+	public float sz;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (sx, sy, sz);
+		
+	}
+}
